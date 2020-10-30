09aac0fe47d6fe735e2c3abb3b1770947994c69b
mngrm3a <3806081-mngrm3a@users.noreply.gitlab.com>
2020-10-30 21:51:57 +0100

    Add primitive command runner implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,83 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec, ExecException, ExecOptions, ExecOptionsWithStringEncoding } from "child_process";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let getOutputCommand = vscode.commands.registerCommand(
+		'shell-command.getOutput',
+		command(
+			s => s.endsWith('\n') ? s.slice(0, s.length - 1) : s,
+			s => s.length > 0,
+			s => Promise.resolve(s)
+		));
+	let selectOutputCommand =
+		vscode.commands.registerCommand(
+			'shell-command.selectOutput',
+			command(
+				s => s.split('\n').filter(s => s !== ''),
+				items => items.length > 0,
+				async items => vscode.window.showQuickPick(items)
+			));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-command-runner" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-command-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-command-runner!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(getOutputCommand, selectOutputCommand);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+interface CommandOptions {
+	command: string
+	encoding?: BufferEncoding;
+	cwd?: string;
+	env?: NodeJS.ProcessEnv;
+}
+
+function command<R>(
+	mapper: (s: string) => R,
+	validator: (r: R) => boolean,
+	selector: (r: R) => Promise<string | undefined>): (commandOptions: CommandOptions) => Promise<string> {
+	return async commandOptions => {
+		let answer;
+
+		try {
+			const result = await execCommand(commandOptions, mapper);
+			if (validator(result.stdout)) {
+				answer = await selector(result.stdout);
+			} else {
+				vscode.window.showErrorMessage(`Command '${commandOptions.command}' did not produce output`);
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				vscode.window.showErrorMessage(error.message);
+				console.debug(error.stack);
+			} else {
+				console.error(error);
+			}
+		}
+
+		return answer ? answer : '';
+	};
+}
+
+async function execCommand<R>(commandOptions: CommandOptions, mapper: (s: string) => R): Promise<{ stdout: R, stderr: R }> {
+	return new Promise((resolve, reject) => {
+		const callback = (error: ExecException | null, stdout: string, stderr: string) => {
+			if (error) {
+				reject(error);
+			}
+			resolve({
+				stdout: mapper(stdout),
+				stderr: mapper(stderr)
+			});
+		};
+
+		const command = commandOptions.command;
+		const execOptions = commandOptions.encoding ?
+			commandOptions as ExecOptionsWithStringEncoding :
+			commandOptions as ExecOptions;
+
+		if (execOptions) {
+			exec(command, execOptions, callback);
+		} else {
+			exec(command, callback);
+		}
+	});
+}
